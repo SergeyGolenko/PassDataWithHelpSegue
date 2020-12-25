35b19c88055b456b97d54cc8bc9a65e3e5b3b89f
SergeyGolenko <1989sergiotay@gmail.com>
2020-12-25 18:26:49 +0200

    pass data with FirstVC to SecondVC and updateLabel

---
+++ PassDataWithHelpSegue/SecondViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SecondViewController.swift
+//  PassDataWithHelpSegue
+//
+//  Created by Сергей Голенко on 25.12.2020.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    @IBOutlet weak var labelSecondVC: UILabel!
+    
+    
+    var newItem = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        labelSecondVC.text = newItem
+    }
+    
+
+
+
+}
